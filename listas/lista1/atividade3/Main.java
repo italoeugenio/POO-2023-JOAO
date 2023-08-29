25c2534c285a165f9b3c1b83e48a7dc94c851608
Ítalo Eugênio <italoeugenio539@gmail.com>
2023-08-29 18:13:57 -0300

    added more informations about case 4

--- listas/lista1/atividade3/Main.java
+++ listas/lista1/atividade3/Main.java
@@ -16,12 +16,14 @@ public class Main {
             System.out.println("5. Buscar item pelo código");
             System.out.println("0. Sair");
             System.out.print("Escolha uma opção: ");
+           
 
             int opcao = scanner.nextInt();
             scanner.nextLine();
 
             switch (opcao) {
                 case 1:
+                    System.out.println("");
                     // Adicionar item
                     System.out.print("Digite o código do item: ");
                     int codigo = scanner.nextInt();
@@ -43,6 +45,7 @@ public class Main {
                     break;
                 case 2:
                     // Remover item
+                    System.out.println("");
                     System.out.print("Digite o código do item a ser removido: ");
                     int codigoRemover = scanner.nextInt();
                     estoque.removerItem(codigoRemover);
@@ -50,6 +53,7 @@ public class Main {
                     break;
                 case 3:
                     // Atualizar quantidade
+                    System.out.println("");
                     System.out.print("Digite o código do item a ser atualizado: ");
                     int codigoAtualizar = scanner.nextInt();
                     System.out.print("Digite a nova quantidade: ");
@@ -59,14 +63,24 @@ public class Main {
                     break;
                     case 4:
                     // Listar itens
+                    System.out.println("");
                     System.out.println("Itens no estoque:");
                     List<Item> itens = estoque.listarItens();
-                    for (Item Item : itens) {
-                        System.out.println(Item.getNome() + " - Quantidade: " + Item.getQuantidade());
+                    for (Item currentItem : itens) {
+                        System.out.println("Código: " + currentItem.getCodigo());
+                        System.out.println("Nome: " + currentItem.getNome());
+                        System.out.println("Descrição: " + currentItem.getDescricao());
+                        System.out.println("Data: " + currentItem.getData());
+                        System.out.println("Quantidade: " + currentItem.getQuantidade());
+                        System.out.println("Preço: " + currentItem.getPreco());
+                        System.out.println(""); 
+                       
                     }
                     break;
+                
                 case 5:
                     // Buscar item pelo código
+                    System.out.println("");
                     System.out.print("Digite o código do item a ser buscado: ");
                     int codigoBuscar = scanner.nextInt();
                     Item itemEncontrado = estoque.buscarItemPorCodigo(codigoBuscar);
@@ -83,9 +97,9 @@ public class Main {
                 default:
                     System.out.println("Opção inválida!");
             }
-            
+
             System.out.println();
         }
-         
+
     }
 }
